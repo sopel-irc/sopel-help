4cfc3567985e94cab6b95139b3f32af6325ee32e
Florian Strzelecki <florian.strzelecki@gmail.com>
2020-03-22 19:53:47 +0100

    help: update .help's doc and readme

--- README.rst
+++ README.rst
@@ -1,3 +1,8 @@
-# sopel-help
+sopel-help
+==========
 
-Sopel plugin ``.help`` command.
+Sopel plugin ``.help`` command::
+
+    [Exirel]: .help help
+    [Sopel]: Generate help for Sopel's commands.
+    [Sopel]: e.g. .help help or .help
--- sopel_help/plugin.py
+++ sopel_help/plugin.py
@@ -5,7 +5,8 @@ from .providers import Base
 
 
 @module.commands('help')
-@module.example('.help')
+@module.example('.help', user_help=True)
+@module.example('.help help', user_help=True)
 def sopel_help(bot, trigger):
     """Generate help for Sopel's commands."""
     base = Base()
